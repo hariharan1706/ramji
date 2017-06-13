367d5bab66f06a6525baabb568f5f453125156c4
hariharan1706 <chinnuiyer17@gmail.com>
2017-06-13 15:52:50 +0530

    Add files via upload

---
+++ multable.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class multab{
+public static multiplytable{
+Scanner s=new Scanner(System.in);
+System.out.println("enter the numbers");
+int n=s.nextInt();
+for(int i=0;i<=10;i++){
+System.out.println(n+"*"+i+"="+n*i);
+}
+}
+}
