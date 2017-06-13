bd6d3e7fe0e9052b81065e7ce892c76d43373fb6
hariharan1706 <chinnuiyer17@gmail.com>
2017-06-13 12:20:17 +0530

    Create positive_or_negative.java

--- positive_or_negative.java
+++ positive_or_negative.java
@@ -6,9 +6,11 @@ public class odd_or_even {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the Number :");
 		int a =s.nextInt();
-		if(a>=0){
+		if(a>0){
 			System.out.println("Number is positive");
 		}
+		else if(a==0){
+			System.out.println("Number is zero");}
 		else{
 			System.out.println("Number is negative");
 		}	
