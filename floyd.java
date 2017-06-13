8a29bce7b4c9e00be1936cd4d66a1a25bdd81b75
hariharan1706 <chinnuiyer17@gmail.com>
2017-06-13 12:09:20 +0530

    Add files via upload

---
+++ floyd.java
@@ -0,0 +1,16 @@
+ class Prime {
+
+	public static void main(String[] args) {
+int rows,i=0,j=0;
+rows=Integer.parseInt(args[0]);
+System.out.println("floyd triangle");
+for(i=0;i<rows;i++)
+{
+System.out.println();
+for(j=0;j<=i;j++)
+{
+System.out.print("*"+" ");
+}}
+}
+}
+
