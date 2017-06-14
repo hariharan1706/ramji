b27cb321e797403f813c766b330a5dadcfc4847c
hariharan1706 <chinnuiyer17@gmail.com>
2017-06-14 12:03:57 +0530

    Add files via upload

---
+++ sumofnum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class sum{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int sum=0;
+for(int i=0;i<=n;i++)
+sum=sum+i;
+System.out.println(sum);
+s.close();
+}
