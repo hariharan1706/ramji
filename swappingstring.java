0bdccaaa34636e3dbc24248e316b34858c7a79f8
hariharan1706 <chinnuiyer17@gmail.com>
2017-06-20 10:24:47 +0530

    Add files via upload

---
+++ swappingstring.java
@@ -0,0 +1,12 @@
+class swapping{
+public static void main(String[] args){
+String original string="abcde";
+char[] c=original String.to charArray();
+char temp=c[0];
+c[0]=c[1];
+c[1]=temp;
+String swapped String=new String(c);
+System.out.println(original String);
+System.out.println(swapped String);
+}
+}
