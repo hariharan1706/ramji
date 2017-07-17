d074ad10cddee5cb7117792fd234b6243ee0eb85
hariharan1706 <chinnuiyer17@gmail.com>
2017-07-17 14:28:37 +0530

    Add files via upload

---
+++ 18.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class numerics{
+public static void main(String[] args){
+String a;
+int count=0,b=0;
+Scanner s=new Scanner(System.in);
+a=s.next();
+for(int i=0;i<a.length();i++){
+b=a.charAt(i);
+if(b>=48&&b<=57){
+count++;
+}
+}
+System.out.println(count);
+}
+}
