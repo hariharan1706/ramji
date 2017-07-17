ff65ea6f7ca2f5ebf19583831dad473c5349d289
hariharan1706 <chinnuiyer17@gmail.com>
2017-07-17 14:17:34 +0530

    Add files via upload

---
+++ 16.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class now{
+public static void main(String[] args)throws Exception
+{
+Scanner sc=new Scanner(System.in);
+String p=new String();
+p=sc.next();
+String q[]=p.split("\\s");
+System.out.println("number of words in a line");
+}
+}
